235ea14ede1db6caa9512252627b18c335a40eca
Amy Tu <siamesebb@gmail.com>
2018-06-10 23:56:55 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -54,6 +54,8 @@ class Song
   
   def self.create_from_filename(filename)
     song = self.new 
-    
+    song.name = filename.split(" - ")[1].chomp(".mp3")
+    song.artist_name = filename.split(" - ")[0]
+    song.create
   end
 end
