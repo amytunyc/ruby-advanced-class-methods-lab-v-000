73820a130c49f46d075fa7db813d75684a315744
Amy Tu <siamesebb@gmail.com>
2018-06-10 23:48:51 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -46,7 +46,7 @@ class Song
   end
   
   def self.new_from_filename(filename)
-    array = filename.split("-")
+    array = filename.split(" - ")
     array[1] = array[1].chomp(".mp3")
     song = self.new 
     song.name = array[1]
